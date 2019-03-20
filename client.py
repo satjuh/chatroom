f318692857c79f8e66675f0988b00f613e82e70d
Juhani Säteri <juhani.sateri@student.tut.fi>
2019-03-20 15:39:25 +0200

    Made barebones server and client 2 thread model

--- client.py
+++ client.py
@@ -1,16 +1,42 @@
 from threading import Thread
-import socket
+from socket import socket, AF_INET, SOCK_STREAM, timeout
 from tkinter import *
 
+# Included while testing
+# Later argv parameters
+HOST = "127.0.0.1"
+PORT = 6000
+alive = True
+
+# Thread class for listening new messages
+class Listen(Thread):
+    def __init__(self, socket, app):
+        Thread.__init__(self)
+        self.socket = socket
+        self.app = app
+
+    def run(self):
+        server.settimeout(3)
+        while alive:
+            try:
+                msg = self.socket.recv(2048).decode("utf-8")
+                print(msg)
+                Gui.insert_msg(self.app, msg)
+            except timeout:
+                continue
+            except OSError:
+                break
 
 
 class Gui(Tk):
     def __init__(self):
         Tk.__init__(self)
+        # Internal variables
+
         # Frame that houses scroll bar and msg_list
         self.frame = Frame(self)
         self.scrollbar = Scrollbar(self)
-        self.msg_list = Listbox(self.frame, height=50, width = 100, yscrollcommand=self.scrollbar.set)
+        self.msg_list = Listbox(self.frame, height=50, width=100, yscrollcommand=self.scrollbar.set)
 
         # Input box for reading user input and saving it to self.msg variable
         self.msg = StringVar()
@@ -18,7 +44,7 @@ class Gui(Tk):
         self.input_box.bind("<Return>", self.send)
 
         # Buttons for closing and sending
-        self.close_button = Button(self, text="Quit", command=quit)
+        self.close_button = Button(self, text="Quit", command=self.quit)
         self.send_button = Button (self, text="Send", command=self.send)
 
         # Packing
@@ -30,18 +56,36 @@ class Gui(Tk):
         self.close_button.pack()
         self.send_button.pack()
 
-
     def send(self, event=NONE):
-        self.msg_list.insert(END, self.msg.get())
-        self.msg.set("")
+        self.insert_msg(self.msg.get())
 
     def quit(self):
+        global alive
+        alive = False
         self.destroy()
 
-
+    def insert_msg(self, msg):
+        self.msg_list.insert(END, msg)
+        self.msg.set("")
 
 
 
 if __name__ == "__main__":
-    app = Gui()
-    app.mainloop()
+    try:
+        app = Gui()
+
+        # Initialize socket
+        server = socket(AF_INET, SOCK_STREAM)
+        server.connect((HOST, PORT))
+        server.settimeout(3)
+
+        # Starting the listening Thread
+        t = Listen(server, app)
+        t.start()
+        app.mainloop()
+
+        # Shutting down
+        server.close()
+        t.join()
+    except ConnectionRefusedError:
+        print("Server is not up on", HOST, PORT)
--- server.py
+++ server.py
@@ -1,4 +1,53 @@
+from socket import socket, AF_INET, SOCK_STREAM, timeout
+from threading import Thread
+from datetime import datetime
+
+list_of_clients = []
+
+# Included while testing
+# Later argv parameters
+HOST = "127.0.0.1"
+PORT = 6000
+alive = True
+
+def broadcast(sender, msg, name):
+    for client in list_of_clients:
+        if client != sender:
+            client.send(name.encode + msg)
+
+def service_client(conn, addr):
+    welcome = "Welcome to the chatroom"
+    conn.send(welcome.encode())
+    conn.settimeout(30)
+    """
+    while alive:
+        try:
+            msg = conn.recv(2048)
+            broadcast(conn, msg, addr)
+        except timeout:
+            continue
+    """
+
+# Save to log who connected and when.
+def log_print(msg):
+    print(datetime.now().time(), msg)
+
+def main():
+    print("Staring the server with", HOST, PORT)
+    with socket(AF_INET, SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.listen()
+        while True:
+            conn, addr = server.accept()
+            log_print(addr)
+            list_of_clients.append(addr)
+            t = Thread(target=service_client(conn, addr), args=[conn, addr])
+            t.start()
+            t.join()
+
+
+
 
 
 if __name__ == "__main__":
-    print("Hello world")
+    main()
